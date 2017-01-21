feb1ed754e3292fcd6561ed86e69c856b346615f
nilton calegari <nilton calegari>
2017-01-21 19:34:30 -0200

    Ajustado o script e prefab de AI Follow Enemy

--- NewUnityProject/Assets/Scripts/EnemyController.cs
+++ NewUnityProject/Assets/Scripts/EnemyController.cs
@@ -7,10 +7,7 @@ public class EnemyController : MonoBehaviour {
     public float speed = 4f;
     public Boundery boundery;
 
-    private Rigidbody rb;
-
     void Start () {
-        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate () {
