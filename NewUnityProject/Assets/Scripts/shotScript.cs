b909cd487c9af71723bc126f0444331ca409d700
Thomas de Azevedo de Souza <thomas.aes.31@hotmail.com>
2017-01-22 02:24:33 -0200

    Adiciona ações
    Cria components e scripts necessarios para movimentar e atirar nos
    inimigos.

---
+++ NewUnityProject/Assets/Scripts/shotScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shotScript : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.velocity = transform.forward * speed;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy") {
+            Destroy (other.gameObject);
+            Destroy (gameObject);
+        }
+
+        if (other.gameObject.tag == "Border") {
+            Destroy (gameObject);
+        }
+    }
+
+}
